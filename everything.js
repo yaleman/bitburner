e1ac163ef2f35c9fd242561b4a4807718604b173
James Hodgkinson <james@terminaloutcomes.com>
2023-12-23 11:13:04 +1000

    works, again, maybe?

--- everything.js
+++ everything.js
@@ -1,98 +1,151 @@
-const badnames = ['darkweb', '0', 0, ".", "home"];
+import { hackNet } from "hacknet.js";
 
-function scpAndRun(ns, serverName) {
-    var processes = ns.ps(serverName);
+async function hackPorts(ns, serverName) {
+    let hackedPorts = 0;
+    await ns.asleep(0);
+    try {
+        if (ns.fileExists("BruteSSH.exe", "home")) {
+            ns.brutessh(serverName);
+            hackedPorts++;
+        }
+        if (ns.fileExists("FTPCrack.exe", "home")) {
+            ns.ftpcrack(serverName);
+            hackedPorts++;
+        }
+        if (ns.fileExists("relaySMTP.exe", "home")) {
+            ns.relaysmtp(serverName);
+            hackedPorts++;
+        }
+        // if (ns.fileExists("HTTPWorm.exe")) {
+        //     ns.httpworm(serverName);
+        //     hackedPorts++;
+        // }
+        // if (ns.fileExists("SQLInject.exe", "home")) {
+        //     ns.sqlinject(serverName);
+        //     hackedPorts++;
+        // }
+    } catch (err) {
+        ns.print(`Couldn't hack ${serverName} due to port hacking err ${err}`);
+    }
+    return hackedPorts;
+}
+
+function isRunning(ns, hostname, process_name) {
+    return ns.ps(hostname).map((procs) => procs.filename == process_name).includes(true)
+}
+
+async function scpAndRun(ns, serverName) {
+    // var processes = ns.ps(serverName);
     var skippableBasic = 0;
     var skippableShare = 0;
-    for (let process of processes) {
-        if (process.filename == "basichack.js") {
-            skippableBasic = 1;
-        }
-        if (process.filename == "share.js") {
-            skippableShare += 1;
-        }
-    };
-    if (skippableBasic == 0) {
-        ns.scp("basichack.js", serverName);
-        ns.exec("basichack.js", serverName, 1, serverName);
+    if (isRunning(ns, serverName, "basichack.js")) {
+        skippableBasic += 1;
     }
-    if (skippableShare < 3) {
-        ns.scp("share.js", serverName);
-        ns.exec("share.js", serverName, 1);
+    if (isRunning(ns, serverName, "share.js")) {
+        skippableShare += 1;
     }
+    // ns.print(`skippableBasic ${skippableBasic}`);
+    // ns.print(`skippableShare ${skippableShare}`);
+
+    try {
+        if (skippableBasic == 0) {
+            ns.scp("basichack.js", serverName, "home");
+            ns.exec("basichack.js", serverName, "1", serverName);
+        }
+    } catch (error) {
+        ns.print(`Failed to exec basichack on ${serverName}: ${error}`);
+    }
+    try {
+        if (skippableShare < 3) {
+            ns.scp("share.js", serverName, "home");
+            ns.exec("share.js", serverName, "1");
+        }
+    } catch (error) {
+        ns.print(`Failed to exec share on ${serverName}: ${error}`);
+    }
+    await ns.asleep(1);
 }
 
 async function spider(ns, serverList) {
     var newlist = serverList.slice();
+    const badnames = ['darkweb', '0', 0, ".", "home"];
+
     try {
         for (var target of serverList) {
             for (var scanTarget of ns.scan(target)) {
                 if (!badnames.includes(scanTarget) && !newlist.includes(scanTarget)) {
-                    newlist.push(scanTarget);
+                    if (ns.getServer(scanTarget).purchasedByPlayer == false) {
+                        newlist.push(scanTarget);
+
+                    }
                 }
             }
         }
-        await ns.sleep(1);
-    } catch {
+        await ns.asleep(1);
+    } catch (err) {
         ns.print(`spider failed on ${serverList}`);
+        ns.print(err);
         await ns.asleep(1);
     }
-    return [...new Set(newlist)];
+    return [...newlist].sort((a, b) => a.toLowerCase().localeCompare(b.toLowerCase()));
+}
+
+function runShareOnHome(ns) {
+    if (!isRunning(ns, "home", "share.js")) {
+        ns.exec("share.js", "home", 4);
+    }
 }
 
 /** @param {NS} ns */
 export async function main(ns) {
     ns.disableLog('ALL');
+
+    runShareOnHome(ns);
     var serverList = ["home"];
+    let foo = ns;
 
     while (true) {
         try {
-            let myHackingLevel = ns.getPlayer().skills.hacking;
-            serverList = await spider(ns, serverList);
+            let myHackingLevel = foo.getPlayer().skills.hacking;
+            serverList = await spider(foo, serverList);
             let hackedServers = [];
             for (let server of serverList) {
                 if (ns.getServerRequiredHackingLevel(server) > myHackingLevel) {
-                    continue;
+                    // ns.print(`Can't hack ${server} yet`);
+
                 }
-                if (!ns.hasRootAccess(server)) {
-                    ns.print(`hi ${server}`);
-                    if (ns.fileExists("BruteSSH.exe")) {
-                        ns.brutessh(server);
+                else if (!ns.hasRootAccess(server)) {
+                    // ns.print(`hi ${server}`);
+                    let serverStats = ns.getServer(server);
+                    var hackedPortsNum = 0;
+                    if (serverStats.numOpenPortsRequired > 0) {
+                        hackedPortsNum = await hackPorts(ns, server);
                     }
-                    if (ns.fileExists("FTPCrack.exe")) {
-                        ns.ftpcrack(server);
-                    }
-                    if (ns.fileExists("relaySMTP.exe")) {
-                        ns.relaysmtp(server);
-                    }
-                    if (ns.fileExists("HTTPWorm.exe")) {
-                        ns.httpworm(server);
-                    }
-                    if (ns.fileExists("SQLInject.exe", "home")) {
-                        ns.sqlinject(server);
-                    }
-                    try {
+                    ns.print(`This is where we nuke ${server} required: ${serverStats.numOpenPortsRequired} have ${hackedPortsNum}`);
+                    if (serverStats.numOpenPortsRequired <= hackedPortsNum) {
                         await ns.asleep(0);
-                        // ns.nuke(server);
-                    } catch (err) {
-                        ns.print(`Could not nuke ${server}: ${err}`);
+                        try {
+                            ns.nuke(server);
+                        } catch (err) {
+                            ns.print(`Could not nuke ${server}: ${err}`);
+                        }
                     }
                 }
                 else if (server != "home" && server != "darkweb") {
                     hackedServers.push(server);
-                    scpAndRun(ns, server);
-
+                    await scpAndRun(ns, server);
                 }
             }
             ns.print(new Date().toISOString());
             ns.print(`hacked servers: ${hackedServers.length}`);
             ns.print(`servers: ${serverList.length}`);
-            ns.print(`sharepower ${ns.getSharePower()}\n\n`);
-            await ns.sleep(100);
+            ns.print(`sharepower ${ns.getSharePower()}\n`);
+            await ns.asleep(100);
         } catch (err) {
             ns.print(`failed!  ${err}`);
-            await ns.sleep(100);
+            await ns.asleep(100);
         }
+        await hackNet(ns);
     }
 
 }
