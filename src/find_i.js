405926ee155e57525c9e18d1b508bec419088d5c
James Hodgkinson <james@terminaloutcomes.com>
2024-01-22 10:14:27 +1000

    prechecks on find_* scripts

--- src/find_i.js
+++ src/find_i.js
@@ -1,7 +1,18 @@
 /** @param {NS} ns */
 export async function main(ns) {
 
+
+    const serverName = "I.I.I.I";
+
     ns.disableLog('ALL');
-    await ns.exec("spiderit.js", "home", 1, "I.I.I.I");
+
+    let stats = ns.getServer(serverName);
+    let myStats = ns.getPlayer();
+    if (stats.requiredHackingSkill > myStats.skills.hacking) {
+        ns.tprint(`### can't hack ${serverName} yet - ${stats.requiredHackingSkill} > ${myStats.skills.hacking} ###`);
+
+    }
+    ns.disableLog('ALL');
+    await ns.exec("spiderit.js", "home", 1, serverName);
 
 }
--- src/find_run4.js
+++ src/find_run4.js
@@ -1,7 +1,17 @@
 /** @param {NS} ns */
 export async function main(ns) {
 
+    const serverName = "run4theh111z";
+
     ns.disableLog('ALL');
-    await ns.exec("spiderit.js", "home", 1, "run4th");
+
+    let stats = ns.getServer(serverName);
+    let myStats = ns.getPlayer();
+    if (stats.requiredHackingSkill > myStats.skills.hacking) {
+        ns.tprint(`### can't hack ${serverName} yet - ${stats.requiredHackingSkill} > ${myStats.skills.hacking} ###`);
+
+    }
+
+    await ns.exec("spiderit.js", "home", 1, serverName);
 
 }
