51f71109067052fa429c2882f784ee93494a7e41
James Hodgkinson <james@terminaloutcomes.com>
2024-01-28 15:41:00 +1000

    shaven dem yakz

--- src/find_i.js
+++ src/find_i.js
@@ -1,17 +1,7 @@
 /** @param {NS} ns */
 export async function main(ns) {
 
-
     const serverName = "I.I.I.I";
-
-    ns.disableLog('ALL');
-
-    let stats = ns.getServer(serverName);
-    let myStats = ns.getPlayer();
-    if (stats.requiredHackingSkill > myStats.skills.hacking) {
-        ns.tprint(`### can't hack ${serverName} yet - ${stats.requiredHackingSkill} > ${myStats.skills.hacking} ###`);
-
-    }
     ns.disableLog('ALL');
     await ns.exec("spiderit.js", "home", 1, serverName);
 
