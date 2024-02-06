313c598abd0e9a0f85ee33a17b2787e1ecdd7047
James Hodgkinson <james@terminaloutcomes.com>
2024-02-07 07:53:12 +1000

    offloading faction join to a separate script

---
+++ src/x_joinfaction.js
@@ -0,0 +1,22 @@
+
+/** @param {NS} ns */
+export async function main(ns) {
+    ns.disableLog('ALL');
+
+
+
+    ["CyberSec", "Netburners", "The Syndicate", "Slum Snakes", "NiteSec", "Speakers for the Dead", "The Black Hand", "BitRunners", "Daedalus"].forEach((faction) => {
+
+        autoFaction(ns, faction);
+    });
+
+    await ns.asleep(100);
+
+
+}
+
+function autoFaction(ns, name) {
+    if (!ns.getPlayer().factions.includes(name)) {
+        ns.singularity.joinFaction(name);
+    }
+}
